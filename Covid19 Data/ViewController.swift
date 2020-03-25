a411391e1e0647786cd6444acf594decfd2dee19
Syed Ahmed <shah_rukh95@hotmail.com>
2020-03-25 01:32:09 -0400

    Initial Commit

---
+++ Covid19 Data/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid19 Data
+//
+//  Created by Syed Ahmed on 2020-03-25.
+//  Copyright © 2020 Syed Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
